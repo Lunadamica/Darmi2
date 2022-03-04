4681e5f50a86afc9f868f498ab8c9368a4ebb4d4
inma <inmamoran25@gmail.com>
2022-03-04 09:33:45 +0100

    Comentado casi todo el codigo + pantalla de ranking

--- core/src/com/darmi/screens/RankingScreen.java
+++ core/src/com/darmi/screens/RankingScreen.java
@@ -11,7 +11,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 
-
 public class RankingScreen extends BaseScreen{
     private Stage stage;
     private Skin skin;
